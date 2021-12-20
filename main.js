c7cb57826e6453fabedda3e06fd7848da345c779
Dionysis Zindros <dionyziz@gmail.com>
2021-12-19 21:47:56 -0800

    New website

---
+++ main.js
@@ -0,0 +1,23 @@
+/*
+const footer = document.getElementsByTagName('footer')[0]
+const NUM_COINS = 7
+const COIN_LEFT = 1290
+const COIN_TOP = 128
+const COIN_WIDTH = 15.9
+const COIN_MARGIN = 33
+
+function createCoins(left, top, width, count) {
+  for (let i = 0; i < count; ++i) {
+    const img = document.createElement('img')
+    img.src = 'images/coin.png'
+    img.width = width
+    img.style.position = 'absolute'
+    img.style.left = (left + width * i) + 'px'
+    img.style.top = top + 'px'
+    footer.appendChild(img)
+  }
+}
+
+createCoins(COIN_LEFT, COIN_TOP, COIN_WIDTH, NUM_COINS)
+createCoins(COIN_LEFT + COIN_WIDTH * NUM_COINS + COIN_MARGIN, COIN_TOP, COIN_WIDTH, NUM_COINS)
+*/
